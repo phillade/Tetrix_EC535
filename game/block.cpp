cdbca1d2251826169fb5f747bf793fcb45bb05cb
juneh <jhua@bu.edu>
2019-05-03 18:58:15 -0400

    comment block.h/cpp

--- game/block.cpp
+++ game/block.cpp
@@ -1,7 +1,394 @@
-//
-// Created by Phillip Teng on 2019-04-15.
-//
-
 #include "block.h"
 
+using namespace std;
+
+block::block(){
+	/* 
+	Constructor for class block. Generates a block of random type & orientation.
+	*/
+
+    color = 0; // default color
+    
+    // randomly generate one of 7 types of blocks
+    type = Type(rand()%7);
+
+    // randomly generate one of 4 orientations
+    orientation = rand()%4;
+
+    // Get coordinates
+    coord = getCoordinates(orientation); // offset by reference pt (references tetris grid)
+	zeroedCoord = getCoordinates(orientation); // no offset
+
+    // Offset by ref point
+    refPt = make_pair(4,-2); // default
+    for (int i=0; i<coord.size(); i++){
+        coord[i].first += refPt.first;
+        coord[i].second += refPt.second;
+    }
+}
+
+vector<pair<int, int> > block::blockICoords(Orientation ori){
+	/* Get coordinates for block I given orientation. 
+	  ARGS:
+		ori (Orientation): Requested orientation 
+
+	  RETURN:
+		coord (vector<pair<int, int> >): Coordinates of block with requested orientation.
+    */
+    vector<pair<int, int> > coord;
+    switch(ori)
+    {
+        case left:
+            coord.push_back(make_pair(0,2));
+            coord.push_back(make_pair(1,2));
+            coord.push_back(make_pair(2,2));
+            coord.push_back(make_pair(3,2));
+            break;
+        case right:
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(2,1));
+            coord.push_back(make_pair(3,1));
+            break;
+        case up:
+            coord.push_back(make_pair(1,0));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(1,2));
+            coord.push_back(make_pair(1,3));
+            break;
+        default: // down
+            coord.push_back(make_pair(2,0));
+            coord.push_back(make_pair(2,1));
+            coord.push_back(make_pair(2,2));
+            coord.push_back(make_pair(2,3));
+    }
+    return coord;
+}
+
+vector<pair<int, int> > block::blockJCoords(Orientation ori){
+	/* Get coordinates for block J given orientation. 
+	  ARGS:
+		ori (Orientation): Requested orientation 
+
+	  RETURN:
+		coord (vector<pair<int, int> >): Coordinates of block with requested orientation.
+   */
+    vector<pair<int, int> > coord;
+    switch(ori)
+    {
+        case left:
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(2,1));
+            coord.push_back(make_pair(2,2));
+            break;
+        case right:
+            coord.push_back(make_pair(0,0));
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(2,1));
+            break;
+        case up:
+            coord.push_back(make_pair(0,2));
+            coord.push_back(make_pair(1,2));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(1,0));
+            break;
+        default: // down
+            coord.push_back(make_pair(1,2));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(1,0));
+            coord.push_back(make_pair(2,0));
+    }
+    return coord;
+}
+
+vector<pair<int, int> > block::blockLCoords(Orientation ori){
+	/* Get coordinates for block L given orientation. 
+	  ARGS:
+		ori (Orientation): Requested orientation 
+
+	  RETURN:
+		coord (vector<pair<int, int> >): Coordinates of block with requested orientation.
+   */
+    vector<pair<int, int> > coord;
+    switch(ori)
+    {
+        case left:
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(2,1));
+            coord.push_back(make_pair(0,2));
+            break;
+        case right:
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(2,1));
+            coord.push_back(make_pair(2,0));
+            break;
+        case up:
+            coord.push_back(make_pair(0,0));
+            coord.push_back(make_pair(1,0));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(1,2));
+            break;
+        default: // down
+            coord.push_back(make_pair(2,2));
+            coord.push_back(make_pair(1,2));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(1,0));
+    }
+    return coord;
+}
+
+vector<pair<int, int> > block::blockOCoords(Orientation ori){
+	/* Get coordinates for block O given orientation. 
+	  ARGS:
+		ori (Orientation): Requested orientation 
+
+	  RETURN:
+		coord (vector<pair<int, int> >): Coordinates of block with requested orientation.
+   */
+    vector<pair<int, int> > coord;
+    switch(ori)
+    {
+        case left:
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(0,2));
+            coord.push_back(make_pair(1,2));
+            break;
+        case right:
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(0,2));
+            coord.push_back(make_pair(1,2));
+            break;
+        case up:
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(0,2));
+            coord.push_back(make_pair(1,2));
+            break;
+        default: // down
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(0,2));
+            coord.push_back(make_pair(1,2));
+    }
+    return coord;
+}
+
+vector<pair<int, int> > block::blockSCoords(Orientation ori){
+	/* Get coordinates for block S given orientation. 
+	  ARGS:
+		ori (Orientation): Requested orientation 
+
+	  RETURN:
+		coord (vector<pair<int, int> >): Coordinates of block with requested orientation.
+   */
+    vector<pair<int, int> > coord;
+    switch(ori)
+    {
+        case left:
+            coord.push_back(make_pair(0,2));
+            coord.push_back(make_pair(1,2));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(2,1));
+            break;
+        case right:
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(1,0));
+            coord.push_back(make_pair(2,0));
+            break;
+        case up:
+            coord.push_back(make_pair(0,0));
+            coord.push_back(make_pair(0,1));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(1,2));
+            break;
+        default: // down
+            coord.push_back(make_pair(1,0));
+            coord.push_back(make_pair(1,1));
+            coord.push_back(make_pair(2,1));
+            coord.push_back(make_pair(2,2));
+    }
+    return coord;
+}
+
+vector<pair<int, int> > block::blockTCoords(Orientation ori){
+	/* Get coordinates for block T given orientation. 
+	  ARGS:
+		ori (Orientation): Requested orientation 
+
+	  RETURN:
+		coord (vector<pair<int, int> >): Coordinates of block with requested orientation.
+   */
+    vector<pair<int, int> > coord;
+    switch(ori)
+    {
+        case left:
+                 coord.push_back(make_pair(0,1));
+                 coord.push_back(make_pair(1,1));
+                 coord.push_back(make_pair(2,1));
+                 coord.push_back(make_pair(1,2));
+            break;
+        case right:
+                 coord.push_back(make_pair(0,1));
+                 coord.push_back(make_pair(1,1));
+                 coord.push_back(make_pair(2,1));
+                 coord.push_back(make_pair(1,0));
+            break;
+        case up:
+                 coord.push_back(make_pair(1,2));
+                 coord.push_back(make_pair(1,1));
+                 coord.push_back(make_pair(1,0));
+                 coord.push_back(make_pair(0,1));
+            break;
+        default: // down
+                 coord.push_back(make_pair(1,2));
+                 coord.push_back(make_pair(1,1));
+                 coord.push_back(make_pair(1,0));
+                 coord.push_back(make_pair(2,1));
+    }
+    return coord;
+}
+
+vector<pair<int, int> > block::blockZCoords(Orientation ori){
+	/* Get coordinates for block Z given orientation. 
+	  ARGS:
+		ori (Orientation): Requested orientation 
+
+	  RETURN:
+		coord (vector<pair<int, int> >): Coordinates of block with requested orientation.
+   */
+    vector<pair<int, int> > coord;
+    switch(ori)
+    {
+        case left:
+                 coord.push_back(make_pair(0,1));
+                 coord.push_back(make_pair(1,1));
+                 coord.push_back(make_pair(1,2));
+                 coord.push_back(make_pair(2,2));
+            break;
+        case right:
+                 coord.push_back(make_pair(0,0));
+                 coord.push_back(make_pair(1,0));
+                 coord.push_back(make_pair(1,1));
+                 coord.push_back(make_pair(2,1));
+            break;
+        case up:
+                 coord.push_back(make_pair(0,2));
+                 coord.push_back(make_pair(0,1));
+                 coord.push_back(make_pair(1,1));
+                 coord.push_back(make_pair(1,0));
+            break;
+        default: // down
+                 coord.push_back(make_pair(1,2));
+                 coord.push_back(make_pair(1,1));
+                 coord.push_back(make_pair(2,1));
+                 coord.push_back(make_pair(2,0));
+    }
+    return coord;
+}
+
+vector<pair<int, int> > block::getCoordinates(int ori){
+	/* Get coordinates for block with specified orientation.
+
+	   ARGS: 
+		ori (int): Orientation of block to get coordinates for.
+
+	   RETURN
+		Coordinates of block with specified orientation.
+   	*/
+    switch(type)
+    {
+        case I:
+            return blockICoords(Orientation(ori));
+        case L:
+            return blockLCoords(Orientation(ori));
+        case J:
+            return blockJCoords(Orientation(ori));
+        case O:
+            return blockOCoords(Orientation(ori));
+        case S:
+            return blockSCoords(Orientation(ori));
+        case Z:
+            return blockZCoords(Orientation(ori));
+        case T:
+            return blockTCoords(Orientation(ori));
+        default:
+            break;
+    }
+}
+
+int block::rotateOrientation(int oldOri, int direction){
+    /*
+     Get orientation of block if rotated. But do not actually rotate block! 
+	 * direction = 0 : rotate right
+     * direction = 1 : rotate left
+
+     ARGS:
+     	oldOri (int): Orientation of block that will be rotated
+     	direction (int): Direction to rotate the block
+
+ 	RETURN:
+ 		newOri (int): Orientation of block after rotation
+    */
+    int newOri;
+    if (direction){
+        if (oldOri == 3)
+            newOri = 0;
+        else
+            newOri = orientation + 1;
+    } else {
+        if (oldOri == 0)
+            newOri = 3;
+        else
+            newOri = orientation - 1;
+    }
+    return newOri;
+}
+
+vector<pair<int, int> > block::getRotateCoord(int direction){
+    /* 
+     Get coordinates of block if rotated. But do not actually rotate block!
+     * direction = 0 : rotate right
+     * direction = 1 : rotate left
+
+     ARGS: 
+     	direction (int): Direction to rotate the block
+
+ 	RETURN:
+ 		Coordinates of rotated block
+     */
+    // Get new orientation
+    int newOri = rotateOrientation(orientation, direction);
+
+    // Get coordinates of rotated block
+    vector<pair<int, int> > newCoord = getCoordinates(newOri);
+
+    // add ref point to coords to calculate true coords on tetris grid
+    for (int i=0; i<newCoord.size(); i++){
+        newCoord[i].first += refPt.first;
+        newCoord[i].second += refPt.second;
+    }
+    return newCoord;
+}
+
+void block::setBlock(int ori, vector<pair<int, int> > newCoord){
+	/*
+		Set block orientation and coordinates.
+
+		ARGS: 
+			ori (int): Orientation to set block to
+			newCoord (vector<pair<int, int> >): Coordinates to set block to
+
+		Return:
+			void
+	*/
+    orientation = ori; // set new orientation
+    coord = newCoord; // set new Coord
+}
 
--- game/block.h
+++ game/block.h
@@ -1,345 +1,73 @@
-//
-// Created by Phillip Teng on 2019-04-15.
-//
+#ifndef TETRIX_EC535_BLOCK_H
+#define TETRIX_EC535_BLOCK_H
+
 #include <utility> // for pair
 #include <iostream> //everything else
 #include <cstdlib> //for rand
 #include <ctime>
 #include <vector>
-//#include <random>
 
-#ifndef TETRIX_EC535_BLOCK_H
-#define TETRIX_EC535_BLOCK_H
-
-#define RIGHT 1
-#define LEFT 0
+#define RIGHT 1 // for rotating left
+#define LEFT 0 // for rotating right
 
 using namespace std;
 
 class block {
-    enum Orientation{
-        left, down, right, up
-    };
 
 public:
-    enum Type {
+    enum Type { // Type of block
         //The explanations to the type of the block is found here
         //https://tetris.fandom.com/wiki/Tetromino
         I, J, L, O, S, T, Z
     };
 
-    //Variables
-    Type type;
-    int orientation;
-    //Orientation orientation;
-    vector<pair<int, int> > coord;
-    vector<pair<int, int> > zeroedCoord;
+    enum Orientation{ // Only for readibility
+        left, down, right, up
+    };  
 
-    pair<int,int> refPt;
+    Type type; // type of block
+    int orientation; // block orientation
+    vector<pair<int, int> > coord; // coords of block w/ offset
+    vector<pair<int, int> > zeroedCoord; // coords of block w/o offset
+    pair<int,int> refPt; // block offset
+    int color; // color of block
 
-    int color;
+    // Generates a block of random orientation and type
+    block();
 
-    vector<pair<int, int> > blockICoords(Orientation ori){
-        vector<pair<int, int> > coord;
-        switch(ori)
-        {
-            case left:
-                coord.push_back(make_pair(0,2));
-                coord.push_back(make_pair(1,2));
-                coord.push_back(make_pair(2,2));
-                coord.push_back(make_pair(3,2));
-                break;
-            case right:
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(2,1));
-                coord.push_back(make_pair(3,1));
-                break;
-            case up:
-                coord.push_back(make_pair(1,0));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(1,2));
-                coord.push_back(make_pair(1,3));
-                break;
-            default: // down
-                coord.push_back(make_pair(2,0));
-                coord.push_back(make_pair(2,1));
-                coord.push_back(make_pair(2,2));
-                coord.push_back(make_pair(2,3));
-        }
-        return coord;
-    };
+    // Get coordinates for block I given orientation
+    vector<pair<int, int> > blockICoords(Orientation ori);;
 
-    vector<pair<int, int> > blockJCoords(Orientation ori){
-        vector<pair<int, int> > coord;
-        switch(ori)
-        {
-            case left:
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(2,1));
-                coord.push_back(make_pair(2,2));
-                break;
-            case right:
-                coord.push_back(make_pair(0,0));
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(2,1));
-                break;
-            case up:
-                coord.push_back(make_pair(0,2));
-                coord.push_back(make_pair(1,2));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(1,0));
-                break;
-            default: // down
-                coord.push_back(make_pair(1,2));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(1,0));
-                coord.push_back(make_pair(2,0));
-        }
-        return coord;
-    };
+    // Get coordinates for block J given orientation
+    vector<pair<int, int> > blockJCoords(Orientation ori);
 
-    vector<pair<int, int> > blockLCoords(Orientation ori){
-        vector<pair<int, int> > coord;
-        switch(ori)
-        {
-            case left:
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(2,1));
-                coord.push_back(make_pair(0,2));
-                break;
-            case right:
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(2,1));
-                coord.push_back(make_pair(2,0));
-                break;
-            case up:
-                coord.push_back(make_pair(0,0));
-                coord.push_back(make_pair(1,0));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(1,2));
-                break;
-            default: // down
-                coord.push_back(make_pair(2,2));
-                coord.push_back(make_pair(1,2));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(1,0));
-        }
-        return coord;
-    };
+    // Get coordinates for block L given orientation
+    vector<pair<int, int> > blockLCoords(Orientation ori);
 
-    vector<pair<int, int> > blockOCoords(Orientation ori){
-        vector<pair<int, int> > coord;
-        switch(ori)
-        {
-            case left:
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(0,2));
-                coord.push_back(make_pair(1,2));
-                break;
-            case right:
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(0,2));
-                coord.push_back(make_pair(1,2));
-                break;
-            case up:
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(0,2));
-                coord.push_back(make_pair(1,2));
-                break;
-            default: // down
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(0,2));
-                coord.push_back(make_pair(1,2));
-        }
-        return coord;
-    };
+    // Get coordinates for block O given orientation
+    vector<pair<int, int> > blockOCoords(Orientation ori);
 
-    vector<pair<int, int> > blockSCoords(Orientation ori){
-        vector<pair<int, int> > coord;
-        switch(ori)
-        {
-            case left:
-                coord.push_back(make_pair(0,2));
-                coord.push_back(make_pair(1,2));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(2,1));
-                break;
-            case right:
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(1,0));
-                coord.push_back(make_pair(2,0));
-                break;
-            case up:
-                coord.push_back(make_pair(0,0));
-                coord.push_back(make_pair(0,1));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(1,2));
-                break;
-            default: // down
-                coord.push_back(make_pair(1,0));
-                coord.push_back(make_pair(1,1));
-                coord.push_back(make_pair(2,1));
-                coord.push_back(make_pair(2,2));
-        }
-        return coord;
-    };
+    // Get coordinates for block S given orientation
+    vector<pair<int, int> > blockSCoords(Orientation ori);
 
-    vector<pair<int, int> > blockTCoords(Orientation ori){
-        vector<pair<int, int> > coord;
-        switch(ori)
-        {
-            case left:
-                     coord.push_back(make_pair(0,1));
-                     coord.push_back(make_pair(1,1));
-                     coord.push_back(make_pair(2,1));
-                     coord.push_back(make_pair(1,2));
-                break;
-            case right:
-                     coord.push_back(make_pair(0,1));
-                     coord.push_back(make_pair(1,1));
-                     coord.push_back(make_pair(2,1));
-                     coord.push_back(make_pair(1,0));
-                break;
-            case up:
-                     coord.push_back(make_pair(1,2));
-                     coord.push_back(make_pair(1,1));
-                     coord.push_back(make_pair(1,0));
-                     coord.push_back(make_pair(0,1));
-                break;
-            default: // down
-                     coord.push_back(make_pair(1,2));
-                     coord.push_back(make_pair(1,1));
-                     coord.push_back(make_pair(1,0));
-                     coord.push_back(make_pair(2,1));
-        }
-        return coord;
-    };
+    // Get coordinates for block T given orientation
+    vector<pair<int, int> > blockTCoords(Orientation ori);
 
-    vector<pair<int, int> > blockZCoords(Orientation ori){
-        vector<pair<int, int> > coord;
-        switch(ori)
-        {
-            case left:
-                     coord.push_back(make_pair(0,1));
-                     coord.push_back(make_pair(1,1));
-                     coord.push_back(make_pair(1,2));
-                     coord.push_back(make_pair(2,2));
-                break;
-            case right:
-                     coord.push_back(make_pair(0,0));
-                     coord.push_back(make_pair(1,0));
-                     coord.push_back(make_pair(1,1));
-                     coord.push_back(make_pair(2,1));
-                break;
-            case up:
-                     coord.push_back(make_pair(0,2));
-                     coord.push_back(make_pair(0,1));
-                     coord.push_back(make_pair(1,1));
-                     coord.push_back(make_pair(1,0));
-                break;
-            default: // down
-                     coord.push_back(make_pair(1,2));
-                     coord.push_back(make_pair(1,1));
-                     coord.push_back(make_pair(2,1));
-                     coord.push_back(make_pair(2,0));
-        }
-        return coord;
-    };
+    // Get coordinates for block Z given orientation
+    vector<pair<int, int> > blockZCoords(Orientation ori);
 
-    vector<pair<int, int> > getCoordinates(int ori){
-        switch(type)
-        {
-            case I:
-                return blockICoords(Orientation(ori));
-            case L:
-                return blockLCoords(Orientation(ori));
-            case J:
-                return blockJCoords(Orientation(ori));
-            case O:
-                return blockOCoords(Orientation(ori));
-            case S:
-                return blockSCoords(Orientation(ori));
-            case Z:
-                return blockZCoords(Orientation(ori));
-            case T:
-                return blockTCoords(Orientation(ori));
-            default:
-                break;
-        }
-    };
+    // Get coordinates for block Object's block type given orientation
+    vector<pair<int, int> > getCoordinates(int ori);
 
-    int rotateOrientation(int oldOri, int direction){
-        /* direction = 0 : rotate right
-         * direction = 1 : rotate left
-         */
-        int newOri;
-        if (direction){
-            if (oldOri == 3)
-                newOri = 0;
-            else
-                newOri = orientation + 1;
-        } else {
-            if (oldOri == 0)
-                newOri = 3;
-            else
-                newOri = orientation - 1;
-        }
-        return newOri;
-    }
+    // Get orientation if block rotated
+    int rotateOrientation(int oldOri, int direction);
 
-    vector<pair<int, int> > getRotateCoord(int direction){
-        /* direction = 0 : rotate right
-         * direction = 1 : rotate left
-         */
-        // Get new orientation
-        int newOri = rotateOrientation(orientation, direction);
+    // Get coordinates if block rotated
+    vector<pair<int, int> > getRotateCoord(int direction);
 
-        // Get base coordinates of rotated block
-        vector<pair<int, int> > newCoord = getCoordinates(newOri);
+    // Set orientation and coordinates of block
+    void setBlock(int ori, vector<pair<int, int> > newCoord);
 
-        // add ref point to base coords to calculate true coords
-        for (int i=0; i<newCoord.size(); i++){
-            newCoord[i].first += refPt.first;
-            newCoord[i].second += refPt.second;
-        }
-        return newCoord;
-    }
-
-    void setBlock(int ori, vector<pair<int, int> > newCoord){
-        orientation = ori; // set new orientation
-        coord = newCoord; // set new Coord
-    }
-
-    //generates a random block
-    block(){
-        color = 0;
-        //randomly generate a number 0 to 6 for the seven types of blocks
-        type = Type(rand()%7);
-        //generate orientation
-        orientation = rand()%4;
-
-        coord = getCoordinates(orientation);
-	zeroedCoord = getCoordinates(orientation);
-
-        // Default ref pt
-        refPt = make_pair(4,-2);
-        for (int i=0; i<coord.size(); i++){
-            coord[i].first += refPt.first;
-            coord[i].second += refPt.second;
-        }
-
-
-    };
 };
 
 
